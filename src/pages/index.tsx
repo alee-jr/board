d0c4f4a0efbf9703bfbf8b56a4dd3370dbb9d3c4
Alexandre Junior <alexandrejr.mc@gmail.com>
2022-07-21 18:56:52 -0300

    feat: adding home content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
 import Head from "next/head";
+import styles from "../styles/styles.module.scss";
 export default function Home() {
   return (
     <>
       <Head>
         <title>Meu Board</title>
       </Head>
-      <div></div>
+      <main className={styles.contentContainer}>
+        <img src="/images/board-user.svg" alt="Ferramenta board" />
+        <section className={styles.callToAction}>
+          <h1>
+            Uma ferramenta para seu dia a dia Escreva, planeje e organiza-se..
+          </h1>
+          <p>
+            <span>100% Gratuita</span> e online.
+          </p>
+        </section>
+        <div className={styles.donaters}>
+          <img
+            src="https://icon-library.com/images/no-user-image-icon/no-user-image-icon-9.jpg"
+            alt="Usuário 1"
+          />
+        </div>
+      </main>
     </>
   );
 }
