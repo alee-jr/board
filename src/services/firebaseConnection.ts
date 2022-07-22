01a2755d45c2277e199ba2cd97a7514eb12fa055
Alexandre Junior <alexandrejr.mc@gmail.com>
2022-07-22 20:23:09 -0300

    feat: adding firebase connection

---
+++ src/services/firebaseConnection.ts
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyACFAYOCEWmNCKJeX3YSTCayi2C_frXANc",
+  authDomain: "boardapp-9745f.firebaseapp.com",
+  projectId: "boardapp-9745f",
+  storageBucket: "boardapp-9745f.appspot.com",
+  messagingSenderId: "491153948281",
+  appId: "1:491153948281:web:c473fd67f880fbb8279ea8",
+  measurementId: "G-ZLDZKNW25K",
+};
+
+if (!firebase.app.length) firebase.initializeApp(firebaseConfig);
+
+export default firebase;
